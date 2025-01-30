0933dbf1eb75f151034a274ff19e623274e4e2a2
Saman Amarasinghe <samana@mit.edu>
2025-01-29 21:38:03 -0500

    up

--- nx586_demon_addon/run.sh
+++ nx586_demon_addon/run.sh
@@ -6,6 +6,7 @@ echo "Starting my custom add-on..."
 CONFIG_FILE="/data/nx584_config.ini"
 DEFAULT_CONFIG_FILE="/app/default_nx584_config.ini"
 
+cp "$DEFAULT_CONFIG_FILE" "$CONFIG_FILE"
 
 # Check if the config file exists
 if [ -f "$CONFIG_FILE" ]; then
@@ -19,7 +20,7 @@ fi
 
 # Example: Start a Python script or service
 ##python3 -m http.server 8080 &
-nx584_server --serial /dev/ttyUSB0 --baud 9600 --config "$CONFIG_FILE" 
+nx584_server --serial /dev/ttyUSB0 --baud 9600 --config "$CONFIG_FILE"  --listen 127.0.0.1 --port 5007
 
 # Keep the process running
 while true; do
